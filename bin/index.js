2be4cc1dda9af4e11e05bb3627727bcaf4149396
Akhilesh Bhatt <88084456+Akhilesh-bhatt@users.noreply.github.com>
2023-09-09 23:09:50 +0530

    accessing package.json using pkg-up

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,24 @@
 #!/usr/bin/env node
-// const arg = require('arg');
-import arg from 'arg';
-import chalk from 'chalk';
+const arg = require('arg');
+const chalk = require('chalk');
+const path = require('path');
+const pkgUp = require('pkg-up');
 
 try {
   const args = arg({
     "--start": Boolean,
     "--build": Boolean
   })
-  // console.log(args);
+  const pkgPath = pkgUp.sync({cwd: process.cwd()});
+  const pkg = require(pkgPath);
+  
+  if(pkg.tool){
+    console.log('Found Configuration', pkg.tool);
+  }
+
+  else{
+    console.log(chalk.yellow('Could not find configuration, Using default'));
+  }
   
   if(args["--start"]){
     console.log(chalk.whiteBright("starting the tool"));
