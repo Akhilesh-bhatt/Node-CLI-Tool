969aeea420d0350ff4fca584cf26078937a63d11
Akhilesh Bhatt <88084456+Akhilesh-bhatt@users.noreply.github.com>
2023-09-09 22:21:05 +0530

    error handling

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,26 @@
 #!/usr/bin/env node
-const arg = require('arg');
+// const arg = require('arg');
+import arg from 'arg';
+import chalk from 'chalk';
 
-const args = arg({
-  "--start": Boolean,
-  "--build": Boolean
-})
-// console.log(args);
+try {
+  const args = arg({
+    "--start": Boolean,
+    "--build": Boolean
+  })
+  // console.log(args);
+  
+  if(args["--start"]){
+    console.log(chalk.whiteBright("starting the tool"));
+  }  
+} catch (e) {
+  console.log(chalk.yellow(e.message));
+  console.log();
+  usage();
+}
 
-if(args["--start"]){
-  console.log("starting the tool");
+function usage() {
+  console.log(`${chalk.whiteBright('tool [CMD]')}
+  ${chalk.greenBright('--start')}\tStarts the app
+  ${chalk.greenBright('--build')}\tBuilds the app`);
 }
